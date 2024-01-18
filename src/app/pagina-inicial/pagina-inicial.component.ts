1512f250b2de07aacef8eb197f7cf5f9410d1ec9
Cesar Soto <zork_690@hotmail.com>
2024-01-17 19:46:44 -0600

    updating pagina-inicial component

--- src/app/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pagina-inicial/pagina-inicial.component.ts
@@ -14,17 +14,7 @@ export class PaginaInicialComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit():void{
-      $("#WAButton")
-      .floatingWhatsApp({
-      phone: '5214271000490', //WhatsApp Business phone number
-               headerTitle: 'Platica con nosotros via WhatsApp!', //Popup Title
-               popupMessage: 'Hola, ¿Cómo puedo ayudarte?', //Popup Message
-               showPopup: true, //Enables popup display
-               buttonImage: '<img src="/assets/imagenes/whatsapp.svg" />', //Button Image
-               //headerColor: 'crimson', //Custom header color
-               //backgroundColor: 'crimson', //Custom background button color
-               position: "right" //Position: left | right
-    });
+      
   }
 
 }
