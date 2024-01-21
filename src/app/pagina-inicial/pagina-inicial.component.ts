af9c712c66c6c558ea52b7c6ced7b43c8be93dc6
Cesar Soto <zork_690@hotmail.com>
2024-01-21 10:04:05 -0600

    adding functionality clickable categories button

--- src/app/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pagina-inicial/pagina-inicial.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
+import { fromEvent, Subscription } from 'rxjs';
 declare let $ : any;
 
 @Component({
@@ -8,13 +9,37 @@ declare let $ : any;
 })
 export class PaginaInicialComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('categoriesButton') categoriesButton: ElementRef;
+  @ViewChild('ulCategories') ulCategories: ElementRef;
+
+  categoriesButtonClicked: Subscription = new Subscription();
+  isShowing:boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit():void{
-      
+      this.getWhenCategoriesButtonIsClicked();
+  }
+
+  private getWhenCategoriesButtonIsClicked(): void{
+    this.categoriesButtonClicked = fromEvent(this.categoriesButton.nativeElement, "click")
+    .subscribe(()=>{
+      if(!this.isShowing){ 
+        this.ulCategories.nativeElement.classList.remove("ulCategories");
+        this.isShowing = true;
+      }else{
+        this.ulCategories.nativeElement.classList.add("ulCategories");
+        this.isShowing = false;
+      }
+    });
+  }
+
+  ngOnDestroy(){
+    //For performance reasons
+    this.categoriesButtonClicked.unsubscribe();
   }
 
 }
