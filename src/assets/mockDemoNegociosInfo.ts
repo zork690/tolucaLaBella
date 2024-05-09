8c174b1cdf870eeacfa40ce38f5be7c437dd5a63
Cesar Soto <zork_690@hotmail.com>
2024-05-09 13:28:36 -0600

    updating json to mock

--- src/assets/mockDemoNegociosInfo.ts
+++ src/assets/mockDemoNegociosInfo.ts
@@ -1,482 +1,81 @@
 export const negociosInfo = [
     {
-        id: 1,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "WhatsApp Image 2023-08-13 at 10.39.35 PM (1).jpeg",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
+        "id": 1,
+        "idNegocio": "1714097862223",
+        "nombre": "césar alberto soto reyes",
+        "telefono": "7224304100",
+        "email": "zork_690@hotmail.com",
+        "ubicacion": {
+          "id": 577,
+          "colonia": "SAN LORENZO TEPALTITLAN",
+          "municipio": "TOLUCA",
+          "codigoPostal": 50010
+        },
+        "descripcion": "Compañia especializada en tecnología de la información",
+        "calle": "5 de mayo",
+        "categoria": "pendiente de categorizar",
+        "nombrEmpresa": "ZORKTECH",
+        "numeroExterior": "53",
+        "imagenes": [
+          {
+            "id": 1,
+            "nombre": "esto.PNG",
+            "idNegocio": "1714097862223"
+          },
+          {
+            "id": 2,
+            "nombre": "WhatsApp Image 2023-08-13 at 10.39.35 PM (1).jpeg",
+            "idNegocio": "1714097862223"
+          }
         ]
-    },
-    {
-        id: 2,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "laChule.jpg",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
+      },
+      {
+        "id": 3,
+        "idNegocio": "1714284932343",
+        "nombre": "Edwin Cruz",
+        "telefono": "7224589734",
+        "email": "edwin.jovanny107@gmail.com",
+        "ubicacion": {
+          "id": 254,
+          "colonia": "NEYRA CHAVEZ ARMANDO FRAC",
+          "municipio": "TOLUCA",
+          "codigoPostal": 50200
+        },
+        "descripcion": "Para el barrio se trae OFFI desde papas a la francesa hasta palomitas de pollo y claro unas buenas micheladas pitufos etc",
+        "calle": "C Ignacio López Rayón",
+        "categoria": "pendiente de categorizar",
+        "nombrEmpresa": "La OFFI",
+        "numeroExterior": "2",
+        "imagenes": [
+          {
+            "id": 3,
+            "nombre": "neon-sign-yellow-beer-mug-260nw-2159930199.webp",
+            "idNegocio": "1714284932343"
+          },
+          {
+            "id": 4,
+            "nombre": "neon-sign-yellow-beer-mug-260nw-2159930199.webp",
+            "idNegocio": "1714284932343"
+          }
         ]
-    },
-    {
-        id: 3,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "buena_fe.jpg",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 4,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "j.jpg",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 5,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 6,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 7,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 8,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 9,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 10,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 11,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 12,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 13,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 14,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    },
-    {
-        id: 15,
-        nombre: "Nombre Negocio",
-        descripcion: "lorem loremLorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis, ex. Eos fuga ipsum a obcaecati sed mollitia autem eaque rem pariatur voluptatum quibusdam, quidem harum ipsam, provident porro in minus.",
-        telefono:"7224304100",
-        imagenes:[
-            {
-                url: "url1",
-                meta: "meta info"
-            },
-            {
-                url: "url2",
-                meta: "meta info"
-            },
-            {
-                url: "url3",
-                meta: "meta info"
-            },
-            {
-                url: "url4",
-                meta: "meta info"
-            },
-            {
-                url: "url5",
-                meta: "meta info"
-            },
-            {
-                url: "url6",
-                meta: "meta info"
-            }
-        ]
-    }
+      },
+      {
+        "id": 5,
+        "idNegocio": "1714407580089",
+        "nombre": "césar alberto soto reyes",
+        "telefono": "7224304101",
+        "email": "zxcdds@gmail.com",
+        "ubicacion": {
+          "id": 233,
+          "colonia": "MERCED",
+          "municipio": "LERMA",
+          "codigoPostal": 52006
+        },
+        "descripcion": "\"Haber\" vamos a probar. Esto es una prueba más, de que ahi vamos.",
+        "calle": "lopéz rayón",
+        "categoria": "pendiente de categorizar",
+        "nombrEmpresa": "LA OFFI DEMO",
+        "numeroExterior": "345",
+        "imagenes": []
+      }
 ];
