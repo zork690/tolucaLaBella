76a65e9319af35dbd5106013bc452fe7594ef491
Cesar Soto <zork_690@hotmail.com>
2024-06-01 16:01:03 -0600

    adding valid property for images and negocios

--- src/assets/mockDemoNegociosInfo.ts
+++ src/assets/mockDemoNegociosInfo.ts
@@ -1,81 +1,88 @@
 export const negociosInfo = [
-    {
+  {
+    "id": 1,
+    "idNegocio": "1714097862223",
+    "nombre": "césar alberto soto reyes",
+    "telefono": "7224304100",
+    "email": "zork_690@hotmail.com",
+    "ubicacion": {
+      "id": 577,
+      "colonia": "SAN LORENZO TEPALTITLAN",
+      "municipio": "TOLUCA",
+      "codigoPostal": 50010
+    },
+    "descripcion": "Compañia especializada en tecnología de la información",
+    "calle": "5 de mayo",
+    "categoria": "pendiente de categorizar",
+    "nombrEmpresa": "ZORKTECH",
+    "numeroExterior": "53",
+    "valid": false,
+    "imagenes": [
+      {
         "id": 1,
+        "nombre": "esto.PNG",
         "idNegocio": "1714097862223",
-        "nombre": "césar alberto soto reyes",
-        "telefono": "7224304100",
-        "email": "zork_690@hotmail.com",
-        "ubicacion": {
-          "id": 577,
-          "colonia": "SAN LORENZO TEPALTITLAN",
-          "municipio": "TOLUCA",
-          "codigoPostal": 50010
-        },
-        "descripcion": "Compañia especializada en tecnología de la información",
-        "calle": "5 de mayo",
-        "categoria": "pendiente de categorizar",
-        "nombrEmpresa": "ZORKTECH",
-        "numeroExterior": "53",
-        "imagenes": [
-          {
-            "id": 1,
-            "nombre": "esto.PNG",
-            "idNegocio": "1714097862223"
-          },
-          {
-            "id": 2,
-            "nombre": "WhatsApp Image 2023-08-13 at 10.39.35 PM (1).jpeg",
-            "idNegocio": "1714097862223"
-          }
-        ]
+        "valid": true
       },
+      {
+        "id": 2,
+        "nombre": "WhatsApp Image 2023-08-13 at 10.39.35 PM (1).jpeg",
+        "idNegocio": "1714097862223",
+        "valid": true
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "idNegocio": "1714284932343",
+    "nombre": "Edwin Cruz",
+    "telefono": "7224589734",
+    "email": "edwin.jovanny107@gmail.com",
+    "ubicacion": {
+      "id": 254,
+      "colonia": "NEYRA CHAVEZ ARMANDO FRAC",
+      "municipio": "TOLUCA",
+      "codigoPostal": 50200
+    },
+    "descripcion": "Para el barrio se trae OFFI desde papas a la francesa hasta palomitas de pollo y claro unas buenas micheladas pitufos etc",
+    "calle": "C Ignacio López Rayón",
+    "categoria": "pendiente de categorizar",
+    "nombrEmpresa": "La OFFI",
+    "numeroExterior": "2",
+    "valid": true,
+    "imagenes": [
       {
         "id": 3,
+        "nombre": "neon-sign-yellow-beer-mug-260nw-2159930199.webp",
         "idNegocio": "1714284932343",
-        "nombre": "Edwin Cruz",
-        "telefono": "7224589734",
-        "email": "edwin.jovanny107@gmail.com",
-        "ubicacion": {
-          "id": 254,
-          "colonia": "NEYRA CHAVEZ ARMANDO FRAC",
-          "municipio": "TOLUCA",
-          "codigoPostal": 50200
-        },
-        "descripcion": "Para el barrio se trae OFFI desde papas a la francesa hasta palomitas de pollo y claro unas buenas micheladas pitufos etc",
-        "calle": "C Ignacio López Rayón",
-        "categoria": "pendiente de categorizar",
-        "nombrEmpresa": "La OFFI",
-        "numeroExterior": "2",
-        "imagenes": [
-          {
-            "id": 3,
-            "nombre": "neon-sign-yellow-beer-mug-260nw-2159930199.webp",
-            "idNegocio": "1714284932343"
-          },
-          {
-            "id": 4,
-            "nombre": "neon-sign-yellow-beer-mug-260nw-2159930199.webp",
-            "idNegocio": "1714284932343"
-          }
-        ]
+        "valid": true
       },
       {
-        "id": 5,
-        "idNegocio": "1714407580089",
-        "nombre": "césar alberto soto reyes",
-        "telefono": "7224304101",
-        "email": "zxcdds@gmail.com",
-        "ubicacion": {
-          "id": 233,
-          "colonia": "MERCED",
-          "municipio": "LERMA",
-          "codigoPostal": 52006
-        },
-        "descripcion": "\"Haber\" vamos a probar. Esto es una prueba más, de que ahi vamos.",
-        "calle": "lopéz rayón",
-        "categoria": "pendiente de categorizar",
-        "nombrEmpresa": "LA OFFI DEMO",
-        "numeroExterior": "345",
-        "imagenes": []
+        "id": 4,
+        "nombre": "neon-sign-yellow-beer-mug-260nw-2159930199.webp",
+        "idNegocio": "1714284932343",
+        "valid": true
       }
+    ]
+  },
+  {
+    "id": 5,
+    "idNegocio": "1714407580089",
+    "nombre": "césar alberto soto reyes",
+    "telefono": "7224304101",
+    "email": "zxcdds@gmail.com",
+    "ubicacion": {
+      "id": 233,
+      "colonia": "MERCED",
+      "municipio": "LERMA",
+      "codigoPostal": 52006
+    },
+    "descripcion": "\"Haber\" vamos a probar. Esto es una prueba más, de que ahi vamos.",
+    "calle": "lopéz rayón",
+    "categoria": "pendiente de categorizar",
+    "nombrEmpresa": "LA OFFI DEMO",
+    "numeroExterior": "345",
+    "valid": false,
+    "imagenes": []
+  }
 ];
