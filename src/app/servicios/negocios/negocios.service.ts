b2b5ea91d571b2ab7347ae7fd1eed673851cd744
Cesar Soto <zork_690@hotmail.com>
2024-06-01 15:47:34 -0600

    creating updateBusiness, updateImages, getNegociosTodos methods

--- src/app/servicios/negocios/negocios.service.ts
+++ src/app/servicios/negocios/negocios.service.ts
@@ -34,9 +34,51 @@ export class NegociosService {
     })); 
   }
 
+  updateBusiness(data: any): Observable<any>  {
+    const headers= new HttpHeaders({
+    'Content-Type': 'application/json'
+    ,'Authorization': 'Bearer elVal0RQu3MeD3M1R3g@LaD@G@n@'});
+    let url = this.negocioUrl+'/negocios/actualizarNegocio';
+    //let url = this.baseUrl+'/negocios/actualizarNegocio';
+    return this.http.post(url, data, {headers: headers}).pipe(map(loginJson => {
+      if (loginJson['s'] === 0) {
+        throw new Error(loginJson['m']);
+      }
+      const result = loginJson["r"]
+      return result as any;
+    })); 
+  }
+
+  updateImages(data: any): Observable<any>  {
+    const headers= new HttpHeaders({
+    'Content-Type': 'application/json'
+    ,'Authorization': 'Bearer elVal0RQu3MeD3M1R3g@LaD@G@n@'});
+    let url = this.negocioUrl+'/negocios/actualizarImagenes';
+    //let url = this.baseUrl+'/negocios/actualizarNegocio';
+    return this.http.post(url, data, {headers: headers}).pipe(map(loginJson => {
+      if (loginJson['s'] === 0) {
+        throw new Error(loginJson['m']);
+      }
+      const result = loginJson["r"]
+      return result as any;
+    })); 
+  }
+
   getNegocios():Observable<any>{
     let url = this.negocioUrl+'/negocios/listarNegocios';
     //let url = this.baseUrl+'/negocios/listarNegocios';
+    return this.http.get(url).pipe(map(response => {
+      if (response['s'] === 0) {
+        throw new Error(response['m']);
+      }
+      const result = response["r"]
+      return result as any;
+    })); 
+  }
+
+  getNegociosTodos():Observable<any>{
+    let url = this.negocioUrl+'/negocios/listarNegociosTodos';
+    //let url = this.baseUrl+'/negocios/listarNegociosTodos';
     return this.http.get(url).pipe(map(response => {
       if (response['s'] === 0) {
         throw new Error(response['m']);
