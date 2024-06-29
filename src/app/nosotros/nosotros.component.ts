a6136a754bb405c7f500d704a1a98eea42ad5961
Cesar Soto <zork_690@hotmail.com>
2024-06-28 19:02:05 -0600

    updating component

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nosotros',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NosotrosComponent implements OnInit {
 
-  constructor() { }
+  constructor(private meta: Meta) { }
 
   ngOnInit(): void {
+    this.meta.updateTag( 
+      { name: "title", content: "Nosotros" }, 
+      "name=title");
+    this.meta.updateTag( 
+      { name: "description", content: "Conoce más sobre nuestro directorio de negocios en Toluca. Descubre nuestra misión y el equipo dedicado a conectar a la comunidad con los mejores comercios y servicios locales." }, 
+      "name=description");
+    this.meta.updateTag( 
+      { name: "keywords", content: "Nosotros, Negocios Toluca, Productos, Servicios" }, 
+      "name=keywords");
   }
 
 }
