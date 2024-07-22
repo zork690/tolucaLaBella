a8c65c65bba060aad02aa24919071cf3b372c06f
Cesar Soto <zork_690@hotmail.com>
2024-07-22 12:04:03 -0600

    adding validations before submit the form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms'; 
+import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { UsuariosService } from '../../app/servicios/usuarios/usuarios.service';
 
 @Component({
@@ -10,29 +10,78 @@ import { UsuariosService } from '../../app/servicios/usuarios/usuarios.service';
 export class LoginComponent implements OnInit {
 
   formGroup: FormGroup;
+  usuarioValidacion: string = "";
+  passwordValidacion: string = "";
 
   constructor(private formBuilder: FormBuilder
     , private usuarioService: UsuariosService) {
 
     this.formGroup = this.formBuilder.group({
-      usuario:["", Validators.required]
-      ,password:["", Validators.required]
+      usuario: ["", [Validators.required, this.validateUser()]],
+      password: ["", Validators.required]
     });
 
-   }
+  }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form:FormGroup){
-    console.log("usuario: ",form.value.usuario);
-    console.log("pass: ", form.value.password);
+  onSubmit(form: FormGroup) {
+    this.validaciones();
+    if (this.formGroup.valid) {
+      console.log("Enviando info...");
+      return;
+      this.usuarioService.login(form).subscribe((result: any) => {
+        console.log("El resultado de inicio de sesión es ", result);
+      }, (responseError) => {
+        console.log("ocurrio un error iniciando sesión ", responseError);
+      });
+    }
+  }
 
-    this.usuarioService.login(form).subscribe((result: any) =>{
-      console.log("El resultado de inicio de sesión es ", result);
-    },(responseError)=>{
-      console.log("ocurrio un error iniciando sesión ", responseError);
-    });
+  get registerFormControl() {
+    return this.formGroup.controls;
+  }
+
+  private validarEmailFormat(inputStr: string): boolean {
+    let regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(inputStr);
+  }
+
+  private validaSoloNumeros(inputStr: string): boolean {
+    let regex = /^[0-9]{10}$/;
+    return regex.test(inputStr);
+  }
+
+  private validateUser(): ValidatorFn {
+    return (control: AbstractControl) => {
+      if (this.validarEmailFormat(control.value)) {
+        return null;
+      } else {
+        if (this.validaSoloNumeros(control.value)) {
+          return null;
+        } else {
+          return { invalidUserForm: true };
+        }
+      }
+    }
+  }
+
+  private validaciones() {
+    this.usuarioValidacion = "";
+    this.passwordValidacion = "";
+    if (this.formGroup.controls.usuario.errors?.required) {
+      this.usuarioValidacion = "Usuario es requerido";
+      return;
+    }
+    if (this.formGroup.controls.usuario.errors?.invalidUserForm) {
+      this.usuarioValidacion = "Usuario parece que no es email o celular válido";
+      return;
+    }
+    if (this.formGroup.controls.password.errors?.required) {
+      this.passwordValidacion = "Contraseña es requerida";
+      return;
+    }
   }
 
 }
