935dd6a1dc2119a7e7e946f132e85b9192f8b3c4
Cesar Soto <zork_690@hotmail.com>
2024-08-26 19:57:44 -0600

    creating activingLinks method

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
-export class PanelComponent implements OnInit {
+export class PanelComponent implements OnInit, AfterViewInit  {
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void{
+    this.activingLinks();
+  }
+
+
+  private activingLinks(): void {
+    let header = document.getElementById("myDIV");
+    let btns = header.getElementsByClassName("nav-item");
+    for (let i = 0; i < btns.length; i++) {
+      btns[i].addEventListener("click", function () {
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+      });
+    }
   }
 
 }
