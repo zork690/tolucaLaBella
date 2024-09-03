a977f7e5d089e7fff499c1b64149c5a599a1d0d6
Cesar Soto <zork_690@hotmail.com>
2024-09-02 22:16:25 -0600

    adding validaciones method

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { RegistroService } from '../../app/servicios/registro/registro.service';
 
 @Component({
@@ -11,38 +11,58 @@ export class RegistroComponent implements OnInit {
 
   formularioRegistro: FormGroup;
   enviado = false;
+  passwordValidacion: string;
 
   constructor(
     private formBuilder: FormBuilder,
     private registroService: RegistroService
 
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.formularioRegistro = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
-  });
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    });
   }
 
   get f() { return this.formularioRegistro.controls; }
 
   onSubmit() {
+    if (!this.validaciones()) return;
     this.enviado = true;
     // stop here if form is invalid
     if (this.formularioRegistro.invalid) {
       return;
     }
 
-    console.log("ENVIANDO: ",this.formularioRegistro.value);
+    console.log("ENVIANDO: ", this.formularioRegistro.value);
     this.registroService.registrarUsuario(this.formularioRegistro.value).subscribe((result) => {
-      console.log("RESULTADO: ", result);
+      console.log("RESULTADO REGISTRANDO USUARIO: ", result);
+      this.registroService.sendEmailVerification(this.formularioRegistro.controls.email.value)
+        .subscribe((result) => {
+          console.log("RESULTADO ENVIANDO EMAIL VERIFICACIÓN: ", result);
+        },
+          (error) => {
+            console.log("ERROR: ENVIANDO EMAIL VERIFICACIÓN", error);
+          });
     },
-    (error) => {
-      console.log("ERROR: ", error);
-    });
+      (error) => {
+        console.log("ERROR REGISTRANDO USUARIO: ", error);
+      });
 
   }
 
+  private validaciones(): boolean {
+    if (this.formularioRegistro.controls.password.value
+      !== this.formularioRegistro.controls.confirmPassword.value) {
+      this.passwordValidacion = "El password y la confirmación no coinciden";
+      return false;
+    } else {
+      this.passwordValidacion = "";
+      return true;
+    }
+  }
 
 }
