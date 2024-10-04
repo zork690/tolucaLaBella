f7d6b5e66148eff563bbb1283b3b89265b12cab9
Cesar Soto <zork_690@hotmail.com>
2024-10-03 19:22:15 -0600

    component to display the details for article

--- src/app/noticias-detalle/noticias-detalle.component.ts
+++ src/app/noticias-detalle/noticias-detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-noticias-detalle',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasDetalleComponent implements OnInit {
 
-  constructor() { }
+
+  idArticulo: string;
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.idArticulo = params['articulo'];
+      console.log("articulo: ", this.idArticulo);
+    });
   }
 
 }
