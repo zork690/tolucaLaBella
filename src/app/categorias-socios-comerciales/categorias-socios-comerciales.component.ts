c9c0e2ad5d3a4dfc1cdade2899875c1a3c1f5bba
Cesar Soto <zork_690@hotmail.com>
2024-10-03 23:17:50 -0600

    implementing gettingSubCategorias method

--- src/app/categorias-socios-comerciales/categorias-socios-comerciales.component.ts
+++ src/app/categorias-socios-comerciales/categorias-socios-comerciales.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SubcategoriasService } from '../servicios/subcategorias/subcategorias.service';
 
 @Component({
   selector: 'app-categorias-socios-comerciales',
@@ -9,15 +11,57 @@ import { ActivatedRoute } from '@angular/router';
 export class CategoriasSociosComercialesComponent implements OnInit {
 
   categoria: string;
-  subcategoria: string;
+  subcategorias: any[];
+
+  negociosInfo: any;
 
   constructor(
     private route: ActivatedRoute
+    , private SpinnerServices: NgxSpinnerService
+    , private subCategoriaService: SubcategoriasService
   ) { }
 
   ngOnInit(): void {
     this.categoria = this.route.snapshot.paramMap.get('categoria');
-    this.subcategoria = this.route.snapshot.paramMap.get('subcategoria');
+    this.gettingSubCategorias();
+
   }
+
+  // PARA PROBAR EN LOCAL
+  private gettingSubCategorias():void{
+    console.log("Obteniendo subcategorias de: ", this.categoria);
+    this.SpinnerServices.show("spinnerSubCategorias");
+    this.subcategorias = [
+      {
+        id: "9",
+        subcategoria: "Cines",
+        categoria: this.categoria,
+        metaDescripcion: "Encuentra los mejores cines en Toluca la Bella. Disfruta de las últimas películas en modernas salas con excelente calidad de sonido e imagen."
+      },
+      {
+        id: "9",
+        subcategoria: "Teatros",
+        categoria: this.categoria,
+        metaDescripcion: "Encuentra los mejores cines en Toluca la Bella. Disfruta de las últimas películas en modernas salas con excelente calidad de sonido e imagen."
+      }
+    ];
+    setTimeout(() => {
+      this.SpinnerServices.hide("spinnerSubCategorias");
+    }, 5000);
+  }
+
+  // PARA PROBAR EN EL BACK
+  /*private gettingSubCategorias(): void {
+    this.SpinnerServices.show("spinnerSubCategorias");
+    console.log("Obteniendo subcategorias de: ", this.categoria);
+    this.subCategoriaService.getSubCategoriasByCategoria(this.categoria).subscribe((result) => {
+      console.log("Subcategorias list: ", result);
+      this.subcategorias = result;
+      this.SpinnerServices.hide("spinnerSubCategorias");
+    }
+      , (error) => {
+        console.log("Ocurrio un error obteniendo las subcategorias: ", error);
+      });
+  }*/
 
 }
