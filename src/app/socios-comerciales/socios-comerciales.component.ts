aec588e7c42c036530aefd39d94d78e4b18f2c61
Cesar Soto <zork_690@hotmail.com>
2024-10-08 12:21:37 -0600

    changing getCategorias method to get from backend

--- src/app/socios-comerciales/socios-comerciales.component.ts
+++ src/app/socios-comerciales/socios-comerciales.component.ts
@@ -38,7 +38,7 @@ export class SociosComercialesComponent implements OnInit, AfterViewInit, OnDest
   }
 
   // PARA PROBAR EN EL BACK
-  /*private getCategorias():void{
+  private getCategorias():void{
     this.SpinnerService.show("spinnerCategorias");
     this.categoriaService.getCategorias().subscribe((result)=>{
       console.log("Categorias: ",result);
@@ -47,10 +47,10 @@ export class SociosComercialesComponent implements OnInit, AfterViewInit, OnDest
     }, (error)=>{
       console.log("Ocurrio un error obteniendo las categorias: ", error);
     });
-  }*/
+  }
 
   // PARA PROBAR EN LOCAL
-  private getCategorias(): void {
+  /*private getCategorias(): void {
     this.SpinnerService.show("spinnerCategorias");
     this.categoriasObj = [
       {
@@ -72,7 +72,7 @@ export class SociosComercialesComponent implements OnInit, AfterViewInit, OnDest
     setTimeout(() => {
       this.SpinnerService.hide("spinnerCategorias");
     }, 4000);
-  }
+  }*/
 
   ngOnDestroy() {
     //For performance reasons
