c8d9acb4ad112cc41f46b22618e049557cf5b844
Cesar Soto <zork_690@hotmail.com>
2024-10-31 20:24:07 -0600

    getting subcategoria from service

--- src/app/subcategorias-socios-comerciales/subcategorias-socios-comerciales.component.ts
+++ src/app/subcategorias-socios-comerciales/subcategorias-socios-comerciales.component.ts
@@ -3,6 +3,8 @@ import { NegociosService } from '../servicios/negocios/negocios.service';
 import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { negociosInfo } from 'src/assets/mockDemoNegociosInfo';
+import { SubcategoriasService } from '../servicios/subcategorias/subcategorias.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-subcategorias-socios-comerciales',
@@ -13,34 +15,46 @@ export class SubcategoriasSociosComercialesComponent implements OnInit {
   
   subcategoria: any;
   subcategoriaString:string;
+  negocios: any;
 
   constructor(
     private route: ActivatedRoute
     , private negocioService: NegociosService
     , private SpinnerServices: NgxSpinnerService
-  ) { }
+    , private subCategoriaService: SubcategoriasService
+    , private router: Router
+  ) {
+    this.subCategoriaService.getSubcategoria.subscribe((subcategoria)=>{
+      this.subcategoria = subcategoria
+    });
+   }
 
   ngOnInit(): void {
     this.subcategoriaString = this.route.snapshot.paramMap.get('subcategoria');
-    this.fetchNegocios();
+    console.log("Subcategoria: ", this.subcategoria);
+    if(JSON.stringify(this.subcategoria) === JSON.stringify({})){
+      this.router.navigate(["directorio-de-negocios"]);
+    }else{
+      this.fetchNegocios();
+    }
   }
 
   // PARA PROBAR EN EL BACK
-  /*private fetchNegocios(): void{
+  private fetchNegocios(): void{
     this.SpinnerServices.show("spinnerNegocioSubCategoria");
-    console.log("Obteniendo negocios de: ", this.subcategoria.subcategoria);
-    this.negocioService.getNegociosBySubCategoria(this.subcategoria.subcategoria).subscribe((result)=>{
+    console.log("Obteniendo negocios de: ", this.subcategoriaString);
+    this.negocioService.getNegociosBySubCategoria(this.subcategoriaString).subscribe((result)=>{
       console.log("Negocios list: ",result);
-      this.negociosInfo = result;
+      this.negocios = result;
       this.SpinnerServices.hide("spinnerNegocioSubCategoria");
     }
     , (error)=>{
       console.log("Ocurrio un error obteniendo los negocios por subcategoria: ",error);
     });
-  }*/
+  }
 
     // PARA PROBAR EN LOCAL
-    private fetchNegocios(): void{
+    /*private fetchNegocios(): void{
       this.SpinnerServices.show("spinnerNegocioSubCategoria");
       this.subcategoria = {
         id: "9",
@@ -52,6 +66,6 @@ export class SubcategoriasSociosComercialesComponent implements OnInit {
       setTimeout(() => {
         this.SpinnerServices.hide("spinnerNegocioSubCategoria");
       }, 6000);
-    }
+    }*/
 
 }
