6fd1c7f2d29755ce844602acd440745fadd51d82
Cesar Soto <zork_690@hotmail.com>
2024-12-19 16:59:26 -0600

    feat: adding authValidRoleService to validating

--- src/app/servicios/auth-guard/auth-guard.service.ts
+++ src/app/servicios/auth-guard/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { Router } from '@angular/router';
+import { AuthValidRoleService } from '../auth-valid-role/auth-valid-role.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +11,11 @@ export class AuthGuardService {
   constructor(
     private authService: AuthService
     , private router: Router
+    , private authValidRoleService : AuthValidRoleService
   ) { }
 
   canActivate(): boolean {
-    if (this.authService.isAuthenticated()) {
+    if (this.authService.isAuthenticated() && this.authValidRoleService.hasValidRoles()) {
       return true;
     } else {
       this.router.navigate(['/login']);
