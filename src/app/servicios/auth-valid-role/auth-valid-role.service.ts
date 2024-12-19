e159a012032bba0a58672c658276fefb29aeaf9f
Cesar Soto <zork_690@hotmail.com>
2024-12-19 16:51:27 -0600

    feat: updating hasValidRoles

--- src/app/servicios/auth-valid-role/auth-valid-role.service.ts
+++ src/app/servicios/auth-valid-role/auth-valid-role.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { PanelSociosService } from '../panel-socios/panel-socios.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +8,9 @@ export class AuthValidRoleService {
 
   constructor(
     private panelService: PanelSociosService
-    , private toastr: ToastrService
   ) { }
 
-  canActivate(): boolean {
+  hasValidRoles(): boolean {
     console.log("validating roles ...");
     let isValid: boolean = false;
     let roles = this.panelService.getRoles(this.panelService.getTokenDecoded());
@@ -23,7 +21,6 @@ export class AuthValidRoleService {
       }
     } else {
       console.log("Hay un problema con los roles de su usuario, favor de llamar a soporte técnico");
-      this.toastr.error("Hay un problema con los roles de su usuario, favor de llamar a soporte técnico")
     }
     return isValid;
   }
