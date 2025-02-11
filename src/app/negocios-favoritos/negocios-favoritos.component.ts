08770f2d94d9cd8f668182213540f2bf1e33a4de
Cesar Soto <zork_690@hotmail.com>
2025-02-10 19:25:49 -0600

    feat: adding some logic to show images properly

--- src/app/negocios-favoritos/negocios-favoritos.component.ts
+++ src/app/negocios-favoritos/negocios-favoritos.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { negociosInfo } from '../../assets/mockDemoNegociosInfo';
 import { NegociosService } from '../servicios/negocios/negocios.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AppConfig } from '../servicios/config/app.config';
 
 @Component({
   selector: 'app-negocios-favoritos',
@@ -11,15 +12,44 @@ import { NgxSpinnerService } from 'ngx-spinner';
 export class NegociosFavoritosComponent implements OnInit {
 
   //negociosInfo: any = negociosInfo;
-  negociosInfo:any;
+  negociosInfo: any;
+  imagenesBasePath: string;
+  apiEndPoint: string;
+
+  public getScreenWidth: any;
+  public getScreenHeight: any;
+
+  public chunks: Array<any>;
+  private imagesByRow: number;
 
   constructor(
     private SpinnerServices: NgxSpinnerService
     , private negocioService: NegociosService
-  ) { }
+    , private config: AppConfig
+  ) {
+    //this.apiEndPoint = this.config.getConfig('apiEndPoint');
+    this.apiEndPoint = "https://backend.zorktech.com.mx";
+    this.imagenesBasePath = this.config.getConfig('pathImages');
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onWindowResize() {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
+    this.settingChunks();
+  }
 
   ngOnInit(): void {
+    this.getScreenWidth = window.innerWidth;
+    this.getScreenHeight = window.innerHeight;
     this.fetchNegociosFavoritos();
+  }
+
+  public gettingWithOfImage(chunks: any): any {
+    let width = (chunks.length * 100) / this.imagesByRow;
+    return {
+      "max-width": `${width}%`
+    };
   }
 
   // PARA PROBAR EN EL BACK
@@ -29,6 +59,7 @@ export class NegociosFavoritosComponent implements OnInit {
       console.log("Negocios favoritos list: ", result);
       this.negociosInfo = result;
       this.SpinnerServices.hide("spinnerFavoritos");
+      this.settingChunks();
     }, (error) => {
       console.log("Ocurrió un error obteniendo los negocios favoritos: ", error);
     });
@@ -41,5 +72,27 @@ export class NegociosFavoritosComponent implements OnInit {
       this.SpinnerServices.hide("spinnerFavoritos");
     }, 5000);
   }*/
+
+  private getChunks(): Array<any> {
+    if (this.negociosInfo) {
+      let numberOfImagesByRow = this.imagesByRow;
+      let numberOfNewNegocios = this.negociosInfo.length;
+      let numberOfTotalRows = Math.ceil(numberOfNewNegocios / numberOfImagesByRow);
+      let chunks = Array.from({ length: numberOfTotalRows }, (_, i) => this.negociosInfo.slice(i * numberOfImagesByRow, (i + 1) * numberOfImagesByRow));
+      return chunks;
+    } else {
+      return undefined;
+    }
+  }
+
+  private settingChunks(): void {
+    if (this.getScreenWidth > 575) {
+      this.imagesByRow = 4;
+      this.chunks = this.getChunks();
+    } else {
+      this.imagesByRow = 1;
+      this.chunks = this.getChunks();
+    }
+  }
 
 }
