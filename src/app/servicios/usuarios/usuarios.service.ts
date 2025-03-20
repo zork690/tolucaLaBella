ae33db7f855092136f9807749f018e70e1903513
Cesar Soto <zork_690@hotmail.com>
2025-03-19 19:53:24 -0600

    feat: editting refresh method

--- src/app/servicios/usuarios/usuarios.service.ts
+++ src/app/servicios/usuarios/usuarios.service.ts
@@ -14,7 +14,7 @@ export class UsuariosService {
   usuarioUrl: string;
   permisoUrl: string;
 
-  headers= new HttpHeaders({
+  headers = new HttpHeaders({
     'Content-Type': 'application/json'
   });
 
@@ -25,31 +25,26 @@ export class UsuariosService {
     this.baseUrl = this.config.getConfig('apiEndPoint');
     this.usuarioUrl = `${this.baseUrl}/${environment.api}`;
     this.permisoUrl = `${this.baseUrl}/${environment.api}`;
-   }
+  }
 
 
-  login(usuarioLogin: any): Observable<any>  {
+  login(usuarioLogin: any): Observable<any> {
     //let url = this.baseUrl+'/usuarios/login';
-    let url = this.usuarioUrl+'/usuarios/login';
-    return this.http.post(url, usuarioLogin, {headers: this.headers}).pipe(map(loginJson => {
+    let url = this.usuarioUrl + '/usuarios/login';
+    return this.http.post(url, usuarioLogin, { headers: this.headers }).pipe(map(loginJson => {
       if (loginJson['s'] === 0) {
         throw new Error(loginJson['m']);
       }
       const result = loginJson["r"]
       return result as any;
-    })); 
+    }));
   }
 
-  refresh(refreshToken: string): Observable<any>  {
+  refresh(refreshToken: string) {
     //let url = this.baseUrl+'/usuarios/refresh?refreshToken=';
-    let url = this.usuarioUrl+'/usuarios/refresh?refreshToken=';
-    return this.http.post(url+refreshToken, {headers: this.headers}).pipe(map(loginJson => {
-      if (loginJson['s'] === 0) {
-        throw new Error(loginJson['m']);
-      }
-      const result = loginJson["r"]
-      return result as any;
-    })); 
+    let url = this.usuarioUrl + '/usuarios/refresh?refreshToken=';
+    return this.http.post(url + refreshToken, { headers: this.headers });
   }
+
 
 }
