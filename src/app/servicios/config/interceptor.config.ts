dc952d52fe482f0f03c547978c37493e84914343
Cesar Soto <zork_690@hotmail.com>
2025-03-19 19:52:01 -0600

    feat: adding verificarSiRefresh method

--- src/app/servicios/config/interceptor.config.ts
+++ src/app/servicios/config/interceptor.config.ts
@@ -1,33 +1,77 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import {AppConfig} from '../config/app.config';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { AppConfig } from '../config/app.config';
+import { catchError, filter, switchMap, take, tap } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+import { UsuariosService } from '../usuarios/usuarios.service';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 
-export class Interceptor implements HttpInterceptor{
+export class Interceptor implements HttpInterceptor {
 
-	token:string;
+  token: string;
+  private isFromRefresh: boolean = false;
+  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
-	constructor( 
-		private config: AppConfig
-	) {
-      }
+  constructor(
+    private config: AppConfig
+    , private authService: AuthService
+    , private usuarioService: UsuariosService
+  ) {
+  }
 
 
- intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
- 	this.token = this.config.getConfig('apiToken');
- 	if (this.token == null) {
- 	  console.log("MI PETICION: ",req);
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.token = this.config.getConfig('apiToken');
+    if (this.token == null) {
+      console.log("MI PETICION: ", req);
       return next.handle(req);
     }
 
-    const modifiedReq = req.clone({ 
+    const modifiedReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${this.token}`),
     });
-    console.log("MI PETICION: ",modifiedReq);
-    return next.handle(modifiedReq);
+    console.log("MI PETICION: ", modifiedReq);
+    return next.handle(modifiedReq)
+      .pipe(catchError((error: HttpErrorResponse) => {
+        console.log("ERROR EN RESPONSE: ", error);
+        console.log("STATUS: ", error.status);
+
+        return this.verificarSiRefresh(modifiedReq, next);
+      }));
   }
+
+  private verificarSiRefresh(request: HttpRequest<any>, next: HttpHandler) {
+    if (!this.isFromRefresh) {
+      this.isFromRefresh = true;
+      this.refreshTokenSubject.next(null);
+
+      const refreshT = this.config.getConfig('refresh');
+
+      if (refreshT) {
+        return this.usuarioService.refresh(refreshT).pipe(
+          switchMap((refreshT: any) => {
+            this.isFromRefresh = false;
+            localStorage.setItem('token', refreshT.accessToken);
+            this.refreshTokenSubject.next(refreshT.accessToken);
+            return next.handle(request);
+          }),
+          catchError((error) => {
+            this.isFromRefresh = false;
+            this.authService.logout();
+            return throwError("ERROR: ", error);
+          })
+        );
+      }
+
+    } else {
+      return this.refreshTokenSubject.pipe(filter(refresh => refresh !== null)
+        , take(1)
+        , switchMap((refresh) => next.handle(request)));
+    }
+  }
+
 }
