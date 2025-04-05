2c5324e8d40d087f88622d008bf8d49ab39864dd
Cesar Soto <zork_690@hotmail.com>
2025-04-04 23:39:31 -0600

    feat: se factoriza canActivate para no cerrar sesión del usuario si el token esta expirado

--- src/app/servicios/auth-guard/auth-guard.service.ts
+++ src/app/servicios/auth-guard/auth-guard.service.ts
@@ -22,8 +22,6 @@ export class AuthGuardService {
     if (this.authService.isAuthenticated() && this.authValidRoleService.hasValidRoles()) {
       return true;
     } else {
-      this.authService.logout();
-      this.router.navigate(['/login']);
       return false;
     }
   }
