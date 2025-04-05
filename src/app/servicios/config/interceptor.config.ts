4555304010d0d98da34e01edaf90beb279094f85
Cesar Soto <zork_690@hotmail.com>
2025-04-04 23:40:52 -0600

    feat: se ajusta intercept para detectar errores 401 y mandar llamar al refresh token

--- src/app/servicios/config/interceptor.config.ts
+++ src/app/servicios/config/interceptor.config.ts
@@ -12,9 +12,9 @@ import { UsuariosService } from '../usuarios/usuarios.service';
 
 export class Interceptor implements HttpInterceptor {
 
-  token: string;
-  private isFromRefresh: boolean = false;
-  private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  //token: string;
+  //private isFromRefresh: boolean = false;
+  //private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
 
   constructor(
     private config: AppConfig
@@ -25,57 +25,49 @@ export class Interceptor implements HttpInterceptor {
 
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    this.token = this.config.getConfig('apiToken');
-    if (this.token == null) {
-      console.log("MI PETICION: ", req);
-      return next.handle(req);
-    }
+    console.log("En interceptor");
 
-    const modifiedReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${this.token}`),
-    });
-    console.log("MI PETICION: ", modifiedReq);
-    return next.handle(modifiedReq)
+    console.log("MI PETICION: ", req);
+
+    return next.handle(req)
       .pipe(catchError((error: HttpErrorResponse) => {
         console.log("ERROR EN RESPONSE: ", error);
         console.log("STATUS: ", error.status);
-
-        return this.verificarSiRefresh(modifiedReq, next);
+        if (error instanceof HttpErrorResponse && error.status === 401) {
+          if (!req.url.includes("/login")
+            && !req.url.includes("/refresh")) {
+            return this.verificarSiRefresh(req, next);
+          } else {
+            this.authService.logout();
+            return throwError(() => error);
+          }
+        } else {
+          if(req.url.includes("/refresh") && error.status === 409){
+            this.authService.logout();
+          }
+          return next.handle(req);
+        }
       }));
   }
 
   private verificarSiRefresh(request: HttpRequest<any>, next: HttpHandler) {
-    if (!this.isFromRefresh) {
-      this.isFromRefresh = true;
-      this.refreshTokenSubject.next(null);
+    console.log("REFRESCANDO TOKEN...");
 
-      const refreshT = this.config.getConfig('refresh');
+    const refreshT = this.config.getConfig('refresh');
 
-      if (refreshT) {
-        return this.usuarioService.refresh(refreshT).pipe(
-          switchMap((refreshT: any) => {
-            this.isFromRefresh = false;
-            localStorage.setItem('token', refreshT.accessToken);
-            this.refreshTokenSubject.next(refreshT.accessToken);
-            return next.handle(request);
-          }),
-          catchError((error) => {
-            this.isFromRefresh = false;
-            this.authService.logout();
-            return throwError("ERROR: ", error);
-          })
-        );
-      }
+    return this.usuarioService.refresh(refreshT).pipe(
+      switchMap((refreshT: any) => {
+        localStorage.setItem('token', refreshT.r.accessToken);
+        console.log("se setio el token nuevo");
+        return next.handle(this.addTokenHeader(request, refreshT.r.accessToken));
+      })
+    );
+  }
 
-    } else {
-      return this.refreshTokenSubject.pipe(filter(refresh => refresh !== null)
-        , take(1)
-        , switchMap((refresh) => next.handle(request)));
-    }
+  private addTokenHeader(req: HttpRequest<any>, token: string) {
+    return req.clone({
+      headers: req.headers.set('Authorization', `Bearer ${token}`),
+    });
   }
 
 }
-
-export const authInterceptorProviders = [
-  { provide: HTTP_INTERCEPTORS, useClass: Interceptor, multi: true }
-];
