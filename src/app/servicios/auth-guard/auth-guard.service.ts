07400d8750c9a72650d007065d434801c82c77e4
Cesar Soto <zork_690@hotmail.com>
2025-04-10 13:12:25 -0600

    fix: se factoriza para mandar a login cuando no este autenticado

--- src/app/servicios/auth-guard/auth-guard.service.ts
+++ src/app/servicios/auth-guard/auth-guard.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
-import { Router } from '@angular/router';
 import { AuthValidRoleService } from '../auth-valid-role/auth-valid-role.service';
 
 @Injectable({
@@ -10,18 +9,14 @@ export class AuthGuardService {
 
   constructor(
     private authService: AuthService
-    , private router: Router
-    , private authValidRoleService : AuthValidRoleService
+    , private authValidRoleService: AuthValidRoleService
   ) { }
 
   canActivate(): boolean {
-    if(this.router.url === "/registrate"){
-      console.log(this.router.url);
-      return true;
-    }
     if (this.authService.isAuthenticated() && this.authValidRoleService.hasValidRoles()) {
       return true;
     } else {
+      this.authService.logout();
       return false;
     }
   }
