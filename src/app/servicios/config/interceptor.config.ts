c1510d58645da45195a83c406a6e0078bee12e7f
Cesar Soto <zork_690@hotmail.com>
2025-04-11 00:51:00 -0600

    fix: se ajusta para que se setie el refresh y el token y para logout cuando refresh manda 409

--- src/app/servicios/config/interceptor.config.ts
+++ src/app/servicios/config/interceptor.config.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse, HttpErrorResponse, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { AppConfig } from '../config/app.config';
-import { catchError, filter, switchMap, take, tap } from 'rxjs/operators';
+import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 import { UsuariosService } from '../usuarios/usuarios.service';
 
@@ -12,10 +12,6 @@ import { UsuariosService } from '../usuarios/usuarios.service';
 
 export class Interceptor implements HttpInterceptor {
 
-  //token: string;
-  //private isFromRefresh: boolean = false;
-  //private refreshTokenSubject: BehaviorSubject<any> = new BehaviorSubject<any>(null);
-
   constructor(
     private config: AppConfig
     , private authService: AuthService
@@ -24,30 +20,28 @@ export class Interceptor implements HttpInterceptor {
   }
 
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log("En interceptor");
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    console.log("MI PETICION: ", req);
+    console.log("En interceptor...");
 
-    return next.handle(req)
+    return next.handle(request)
       .pipe(catchError((error: HttpErrorResponse) => {
         console.log("ERROR EN RESPONSE: ", error);
-        console.log("STATUS: ", error.status);
         if (error instanceof HttpErrorResponse && error.status === 401) {
-          if (!req.url.includes("/login")
-            && !req.url.includes("/refresh")) {
-            return this.verificarSiRefresh(req, next);
+          if (!request.url.includes("/login")
+            && !request.url.includes("/refresh")) {
+            return this.verificarSiRefresh(request, next);
           } else {
             this.authService.logout();
-            return throwError(() => error);
           }
         } else {
-          if(req.url.includes("/refresh") && error.status === 409){
+          if (request.url.includes("/refresh") && error.status === 409) {
             this.authService.logout();
           }
-          return next.handle(req);
+          return next.handle(request);
         }
       }));
+
   }
 
   private verificarSiRefresh(request: HttpRequest<any>, next: HttpHandler) {
@@ -58,7 +52,8 @@ export class Interceptor implements HttpInterceptor {
     return this.usuarioService.refresh(refreshT).pipe(
       switchMap((refreshT: any) => {
         localStorage.setItem('token', refreshT.r.accessToken);
-        console.log("se setio el token nuevo");
+        localStorage.setItem('refreshToken', refreshT.r.refreshToken);
+        console.log("se setio el token y el refresh nuevo");
         return next.handle(this.addTokenHeader(request, refreshT.r.accessToken));
       })
     );
