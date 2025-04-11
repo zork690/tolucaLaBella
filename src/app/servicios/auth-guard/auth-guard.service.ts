b682273649a252c29ddcb43d4ed03f84af703303
Cesar Soto <zork_690@hotmail.com>
2025-04-11 00:15:05 -0600

    fix: se agrega if para que no se redirija a login

--- src/app/servicios/auth-guard/auth-guard.service.ts
+++ src/app/servicios/auth-guard/auth-guard.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { AuthValidRoleService } from '../auth-valid-role/auth-valid-role.service';
+import { AppConfig } from '../config/app.config';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,17 @@ export class AuthGuardService {
   constructor(
     private authService: AuthService
     , private authValidRoleService: AuthValidRoleService
+    , private config: AppConfig
   ) { }
 
   canActivate(): boolean {
     if (this.authService.isAuthenticated() && this.authValidRoleService.hasValidRoles()) {
       return true;
     } else {
-      this.authService.logout();
+      if(!this.config.getConfig('apiToken')){
+        console.log("no estaba logueado");
+        this.authService.logout();
+      }
       return false;
     }
   }
