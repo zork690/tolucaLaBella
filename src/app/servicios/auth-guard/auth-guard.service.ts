e804da4431256c4bea269c58264fe439a0891943
Cesar Soto <zork_690@hotmail.com>
2025-04-12 10:09:24 -0600

    fix: se arregla cuando caduca token y no esta en panel-socios

--- src/app/servicios/auth-guard/auth-guard.service.ts
+++ src/app/servicios/auth-guard/auth-guard.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { AuthValidRoleService } from '../auth-valid-role/auth-valid-role.service';
 import { AppConfig } from '../config/app.config';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -12,14 +14,14 @@ export class AuthGuardService {
     private authService: AuthService
     , private authValidRoleService: AuthValidRoleService
     , private config: AppConfig
+    , private location: Location
   ) { }
 
   canActivate(): boolean {
     if (this.authService.isAuthenticated() && this.authValidRoleService.hasValidRoles()) {
       return true;
     } else {
-      if(!this.config.getConfig('apiToken')){
-        console.log("no estaba logueado");
+      if(!this.location.path().includes("panel-socios")){
         this.authService.logout();
       }
       return false;
