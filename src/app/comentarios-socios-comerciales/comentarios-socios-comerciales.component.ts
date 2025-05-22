cd22f607d7356214dab70d0f87cde1f64c271277
Cesar Soto <zork_690@hotmail.com>
2025-05-22 10:42:18 -0600

    feat: se agrega comentario y validaciones para ser enviadas

--- src/app/comentarios-socios-comerciales/comentarios-socios-comerciales.component.ts
+++ src/app/comentarios-socios-comerciales/comentarios-socios-comerciales.component.ts
@@ -1,6 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { NegociosService } from '../servicios/negocios/negocios.service';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { ModalDismissReasons, NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../servicios/auth/auth.service';
 
 @Component({
   selector: 'app-comentarios-socios-comerciales',
@@ -13,31 +16,126 @@ export class ComentariosSociosComercialesComponent implements OnInit {
   public comentariosList: Array<any>;
 
   public isLoaded: boolean = false;
+  modalOptions: NgbModalOptions;
+  closeResult: string;
+  comentarioNegocioFormGroup: FormGroup;
+  private comentarioRequerido: string = "La reseña es requerida";
+  private comentarioCaracteresInvalidos: string = "La reseña tiene caracteres no válidos";
+
+  private valorComentario: string;
 
   constructor(
     private negocioService: NegociosService
     , private SpinnerServices: NgxSpinnerService
-  ) { }
+    , private modalService: NgbModal
+    , private fb: FormBuilder
+    , private auth: AuthService
+  ) {
+
+    this.modalOptions = {
+      backdrop: 'static',
+      backdropClass: 'customBackdrop'
+    };
+
+  }
 
   ngOnInit(): void {
     console.log("obteniendo comentarios del negocio id: ", this.idNegocio);
+
+    this.comentarioNegocioFormGroup = this.fb.group({
+      comentarioFormField: [""]
+    });
+
     this.getComentarios();
   }
 
+  public abrirModal(content) {
+    this.comentarioNegocioFormGroup.reset();
+    this.modalService.open(content, this.modalOptions).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
+  public onSubmitForm() {
 
-  private getComentarios():void{
-    this.SpinnerServices.show("spinnerComentarios");
-      this.negocioService.getNegocioComentarios(this.idNegocio).subscribe((result)=>{
-        console.log("comentarios: ",result);
-        this.comentariosList = result;
-        this.SpinnerServices.hide("spinnerComentarios");
-        this.isLoaded = true;
+    //this.enviado = true;
+    this.validacionesComentarioForm();
+
+    if (this.comentarioNegocioFormGroup.valid) {
+      if (!this.auth.isAuthenticated()) {
+        console.log("Mandando pantalla para que se loguie...");
+        this.abrirModal();
       }
-      , (error)=>{
+      console.log("Enviando comentario: ", this.payloadForAddComment());
+      /*this.SpinnerService.show();
+      this.misArticulosService.createImages(this.payloadForAddingImages())
+        .subscribe((result: any) => {
+          this.SpinnerService.hide();
+          console.log("Enviando imagenes: ", result);
+          this.toastr.success("Imagenes enviadas exitosamente.");
+          this.messageFromChild.emit("1");
+        }, (responseError) => {
+          this.SpinnerService.hide();
+          console.log("ocurrio un error enviando las imágenes ", responseError);
+          this.toastr.error("Error al enviar las imágenes: ", responseError.error.m)
+        });*/
+    }
+  }
+
+
+  private getComentarios(): void {
+    this.SpinnerServices.show("spinnerComentarios");
+    this.negocioService.getNegocioComentarios(this.idNegocio).subscribe((result) => {
+      console.log("comentarios: ", result);
+      this.comentariosList = result;
+      this.SpinnerServices.hide("spinnerComentarios");
+      this.isLoaded = true;
+    }
+      , (error) => {
         this.SpinnerServices.hide("spinnerComentarios");
         console.log("Ocurrio un error obteniendo los comentarios del negocio: ", error);
       });
+  }
+
+  /* funcion para capturar accion de teclado en modal */
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
     }
+  }
+
+  private validacionesComentarioForm() {
+    const comentarioTextArea = this.comentarioNegocioFormGroup.controls.comentarioFormField;
+    const regex: RegExp = new RegExp('^[a-zA-ZáéíóúÁÉÍÓÚñÑ$%\"!0-9()?¿=.,#& ]+$');
+
+    if (comentarioTextArea.value == null
+      || comentarioTextArea.value == "") {
+      document.getElementById("validacion").innerText = this.comentarioRequerido;
+      comentarioTextArea.setErrors(Validators.required);
+    } else {
+      if (!regex.test(comentarioTextArea.value)) {
+        document.getElementById("validacion").innerText = this.comentarioCaracteresInvalidos;
+        comentarioTextArea.setErrors(Validators.pattern('^[a-zA-ZáéíóúÁÉÍÓÚñÑ$%\"!0-9()?¿=.,#& ]+$'));
+      } else {
+        this.valorComentario = comentarioTextArea.value;
+        document.getElementById("validacion").innerText = "";
+      }
+    }
+
+  }
+
+  private payloadForAddComment() {
+    let payload = {
+      idNegocio: this.idNegocio,
+      comentario: this.valorComentario
+    };
+    return JSON.stringify(payload);
+  }
 
 }
